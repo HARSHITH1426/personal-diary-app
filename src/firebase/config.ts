977112fb15bac572084565a43ac4b12c66a15baa
HARSHITH VELDOOTI (RA2411030050036) <hv1147@srmist.edu.in>
2025-10-26 06:14:07 +0000

    delete gmail thing

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-3113629158-6f304",
-  "appId": "1:790746318877:web:781bad72973c5ac73edef4",
-  "apiKey": "AIzaSyCmB7deMljIBEchsoDpVkQ6B7gpCDJowkQ",
-  "authDomain": "studio-3113629158-6f304.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "790746318877"
-};
